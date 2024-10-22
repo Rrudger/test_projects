894c57db26e240574ffb4b6b8f4c8c3ec5b24915
Rrudger <soulo@yandex.ru>
2024-10-22 16:11:08 +0200

    update react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
 import './App.css';
-import { BrowserRouter, HashRouter, Routes, Route, Router, Switch } from 'react-router-dom';
+import { BrowserRouter, createBrowserRouter, RouterProvider, Routes, Route } from 'react-router-dom';
 import MainPage from './components/MainPage.jsx';
 import Page404 from './components/Page404.jsx';
 import FileLoader from './fileLoaderLand/FileLoader.jsx';
 import BusinessCard from './businessCard/BusinessCard.jsx';
 
 function App() {
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainPage />,
+    errorElement: <Page404 />,
+  },
+]);
+
   return (
-    <Router>
-    <Switch>
-          <Route exact path="/">
-            <MainPage />
-          </Route>
-          <Route path="/fileLoader">
-            <FileLoader />
-          </Route>
-        </Switch>
-        </Router>
-      
+     <RouterProvider router={router} />
   );
 }
 
